9b866273b50fe015515013295305d8575e484a53
tngoro19 <47113233+tngoro19@users.noreply.github.com>
2019-01-29 11:46:54 +0000

    Add files via upload

---
+++ PIN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Num();
+        }
+        static void Num()
+        {
+            Console.WriteLine("Welecome to pin number ");
+            string Guess;
+            Guess = Console.ReadLine();
+           
+        }
+    }
+
+}
---
+++ Speed camera.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time();
+        }
+        static void Time()
+        {
+            string time;
+            Console.WriteLine("Welcome to speed camera enter your time  ");
+            time = Console.ReadLine();
+            int length;
+            length = Convert.ToInt32(time);// convert the number to an integer
+            float distance = 1500;
+            float speed1;
+            speed1 = 1500 / length;//performs calculation 
+           
+            double MPH;
+            double mph = (2.23);
+            MPH = speed1 * mph;
+            Console.Write(MPH); //Outputs the speed of the vehicle 
+            Console.Write("the car is travellling");
+            Console.ReadKey();
+            
+
+        }
+    }
+}
